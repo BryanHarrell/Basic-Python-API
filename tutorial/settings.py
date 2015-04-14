2c77cfa631d8b4a3f57b1542d19bda5560f0b7b6
Bryan Harrell <bryan@lifeofbrybry.com>
2015-04-14 11:01:53 +0200

    URL Change once more

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -101,4 +101,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = 'https://limitless-sierra-8332.herokuapp.com/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
