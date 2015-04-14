2f0d5302eba40281c27a7e3a9d3e11a7cc171500
Bryan Harrell <bryan@lifeofbrybry.com>
2015-04-14 13:07:20 +0200

    Added Routing for Home Page, Better Visuals

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,11 +1,21 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'snippets', UserViewSet)
 urlpatterns = [
     # Examples:
     # url(r'^$', 'tutorial.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include('snippets.urls')),
 ]
