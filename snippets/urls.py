3f0ff42c83a3f7b98b2bc23f34b34ab0d34f60b2
Bryan Harrell <bryan@lifeofbrybry.com>
2015-04-15 14:16:06 +0200

    Fixed static root

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,8 +5,7 @@ from django.conf import settings
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
-urlpatterns += urlpatterns('',
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-)
+
 
