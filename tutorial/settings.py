255669fb6ae6c9ee10fe31da3c57276379a64559
Bryan Harrell <bryan@lifeofbrybry.com>
2015-04-15 12:45:17 +0200

    Fixed Procfile and static file display

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -107,3 +107,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
