fc547f45631aea48de7fc662a0c248079fa91243
Bryan Harrell <bryan@lifeofbrybry.com>
2015-04-15 13:16:35 +0200

    fixed settings

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -107,7 +107,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
